cbbc1b79224a4b8d71869a5313413d8682c7f8d3
Thibault <thibault.herve1@gmail.com>
2023-12-05 10:05:17 +0100

    test

---
+++ day4/3_part2.py
@@ -0,0 +1,18 @@
+import re
+
+sum = 0
+
+with open("input.txt") as file:
+    for line in file:
+        splt = re.split(r":|\|", line)
+        winning_nums = re.findall(r"\d+", splt[1])
+        my_nums = re.findall(r"\d+", splt[2])
+
+        point = 0
+
+        for num in my_nums:
+            if num in winning_nums:
+
+        sum += point
+
+print(sum)
